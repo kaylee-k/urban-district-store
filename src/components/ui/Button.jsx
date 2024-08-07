a84d58e2b6e368203aa44681cd99c9c0d20ac288
kaylee-k <kaylee.kihm@gmail.com>
2024-08-07 14:02:09 -0600

    Feat: Enhance UI for new product registration page and add popup notification for successful product addition

--- src/components/ui/Button.jsx
+++ src/components/ui/Button.jsx
@@ -3,8 +3,10 @@ import React from 'react';
 export default function Button({ text, onClick }) {
   return (
     <button
-      className='bg-brand py-1 px-3 rounded-sm hover:brightness-110 
-      transition-all duration-300 hover:scale-110 text-gray-100'
+      className='bg-brand py-1 px-3 rounded-sm 
+      hover:brightness-110 
+      transition-all duration-300 hover:scale-105
+       text-gray-100 text-lg'
       onClick={onClick}
     >
       {text}
--- src/pages/NewProduct.jsx
+++ src/pages/NewProduct.jsx
@@ -6,6 +6,9 @@ import { addNewProduct } from '../api/firebase';
 export default function NewProduct() {
   const [product, setProduct] = useState({});
   const [file, setFile] = useState();
+  const [isUploading, setIsUploading] = useState(false);
+  const [success, setSuccess] = useState(null);
+  const [showPopup, setShowPopup] = useState(false);
   const handleChange = (e) => {
     const { name, value, files } = e.target;
     if (name === 'file') {
@@ -16,15 +19,41 @@ export default function NewProduct() {
   };
   const handleSubmit = (e) => {
     e.preventDefault();
-    uploadImage(file).then((url) => {
-      addNewProduct(product, url);
-    });
+    setIsUploading(true);
+    uploadImage(file)
+      .then((url) => {
+        addNewProduct(product, url).then(() => {
+          setSuccess('Product has been successfully added.');
+          setShowPopup(true);
+          setTimeout(() => {
+            setShowPopup(false);
+            setSuccess(null);
+          }, 4000);
+        });
+      })
+      .finally(() => setIsUploading(false));
   };
 
   return (
-    <section>
-      {file && <img src={URL.createObjectURL(file)} alt='local file' />}
-      <form onSubmit={handleSubmit}>
+    <section className='w-full text-center'>
+      <h2 className='text-2xl font-semibold my-4 py-4 bg-pink-100'>
+        Add New Product
+      </h2>
+      {showPopup && (
+        <div className='fixed inset-0 flex items-center justify-center z-50'>
+          <div className='bg-pink-400 p-4 rounded shadow-lg text-xl font-bold text-white'>
+            <p>🤍 {success}</p>
+          </div>
+        </div>
+      )}
+      {file && (
+        <img
+          className='w-80 mx-auto mb-2'
+          src={URL.createObjectURL(file)}
+          alt='local file'
+        />
+      )}
+      <form className='flex flex-col px-12' onSubmit={handleSubmit}>
         <input
           type='file'
           accept='image/*'
@@ -72,7 +101,10 @@ export default function NewProduct() {
           required
           onChange={handleChange}
         />
-        <Button text={'Add Product'} />
+        <Button
+          text={isUploading ? 'Uploading...' : 'Add Product'}
+          disabled={isUploading}
+        />
       </form>
     </section>
   );
